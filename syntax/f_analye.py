f1e95753fccf33ad13921e633da4bf02c26fca86
fjesser <felixjonas.esser@googlemail.com>
2020-04-16 20:53:02 +0200

    first python file from Felix

---
+++ syntax/f_analye.py
@@ -0,0 +1,27 @@
+# Felix hotel demand analysis
+import pandas as pd
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 500)
+
+# load data
+df = pd.read_csv('./../input/hotel_bookings.csv')
+
+#inspect
+#df.info()  # structure in R
+# df.index  # Zeilen ausgeben (besondere Python Art)
+# df.columns  # Spalten ausgeben
+
+print(df["hotel"].unique())
+print(df["reservation_status"].value_counts())
+
+for variable in df.columns:
+    if df[variable].dtypes == "object":
+        print(df[variable].value_counts())
+
+
+
+
+
+#print(df.describe(include='all'))
+
+
