2f3ba31c1cd92516a80334dca3dd3a6b8e8fe3f6
fjesser <felixjonas.esser@googlemail.com>
2020-04-17 17:34:47 +0200

    Added arrival_date variable in date format

--- syntax/c_analyse.py
+++ syntax/c_analyse.py
@@ -1,1 +1,3 @@
 # Analysis of hotel demand data set
+df['arrival_date'] = df.arrival_date_year.astype(str) + df.arrival_date_month + df.arrival_date_day_of_month.astype(str)
+df['arrival_date'] = pd.to_datetime(df.arrival_date, format="%Y%B%d")
