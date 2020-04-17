5fd91d69946dd58f1f13644f6ecfd207a575b464
Manuel Hollmann <holly-hollmann@hotmail.de>
2020-04-17 17:36:13 +0200

    Added function to correct meals to c_analyse.py and created multiple functions in m_see_data.py

--- syntax/c_analyse.py
+++ syntax/c_analyse.py
@@ -1,1 +1,11 @@
 # Analysis of hotel demand data set
+def fix_meal_manifestations(dataset):
+    '''
+    Function adds undefined meals to SC meals (according to kaggle, Undefined/SC - no meal)
+    Input:
+      dataset: pd.dataFrame object
+    Returns:
+      dataset: pd.dataFrame object in which SC = Undefined + SC from input
+    '''
+    dataset['meal'] = hotel_data['meal'].replace(to_replace="Undefined", value="SC")
+    return(dataset)
--- syntax/m_see_data.py
+++ syntax/m_see_data.py
@@ -1,15 +1,83 @@
+from datetime import date
 import pandas as pd
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 
+
+# Load hotel data set from input folder
 hotel_data = pd.read_csv("../input/hotel_bookings.csv")
 
-# described_data = hotel_data.describe()
-#
-# described_data.to_csv("../output/description.csv")
 
-# print(hotel_data.describe(include="all"))
+def export_dataset_description_to_csv(dataset):
+    '''
+    Function takes description of dataset and saves it as csv
+    Input:
+      dataset: pd.dataFrame object
+    Returns:
+      nothing, saves csv-file
+    '''
+    described_data = dataset.describe(include="all")
+    described_data.to_csv("../output/description.csv")
 
-for variable in hotel_data.columns:
-    if hotel_data[variable].dtype == 'object':
-        print(hotel_data[variable].value_counts())
+
+def print_occurence_of_object_types(dataset):
+    '''
+    Function prints how often each manifestation of non-numeric columns appears
+    Input:
+      dataset: pd.dataFrame object
+    Returns:
+      nothing, prints to console
+    '''
+    for variable in dataset.columns:
+        if dataset[variable].dtype == 'object':
+            print(dataset[variable].value_counts())
+
+
+# print_occurence_of_object_types(hotel_data)
+
+def fix_meal_manifestations(dataset):
+    '''
+    Function adds undefined meals to SC meals (according to kaggle, Undefined/SC - no meal)
+    Input:
+      dataset: pd.dataFrame object
+    Returns:
+      dataset: pd.dataFrame object in which SC = Undefined + SC from input
+    '''
+    dataset['meal'] = hotel_data['meal'].replace(to_replace="Undefined", value="SC")
+    return(dataset)
+
+
+def convert_date(row):
+    '''
+    Function creates a date object by reading the year, month and date of arrival per row
+    Input:
+      dataset: a row of the hotel dataset as pd.Series
+    Returns:
+      dataset: a date object including the arrival date
+    '''
+    year = row['arrival_date_year']
+    month = row['arrival_date_month']
+    day = row['arrival_date_day_of_month']
+
+    month = month.capitalize()
+    months_string = ['January', 'February', 'March', 'April', 'May', 'June',
+                     'July', 'August', 'September', 'October', 'November', 'December']
+    months_numeric = [i for i in range(1, 13)]
+    months = dict(zip(months_string, months_numeric))
+
+    numeric_date = date(year, months[month], day)
+    return(numeric_date)
+
+
+''' My try to get the arrival_date as a date object. Uses apply and is thus really slow (seconds).
+    For optimization, read this link: https://stackoverflow.com/questions/52673285/performance-of-pandas-apply-vs-np-vectorize-to-create-new-column-from-existing-c
+
+    hotel_data['arrival_date'] = hotel_data.apply(convert_date, axis=1)
+'''
+
+
+# hotel_data['arrival_date'] = hotel_data.apply(convert_date, axis=1, args=(
+# hotel_data['arrival_date_year'], hotel_data['arrival_date_month'], hotel_data['arrival_date_day_of_month']))
+
+
+print(hotel_data.head())
