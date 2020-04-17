241bddd847ecbd523f72642561d97717e7d048ce
fjesser <felixjonas.esser@googlemail.com>
2020-04-17 18:29:35 +0200

    Created arrival date variable

--- syntax/f_analye.py
+++ syntax/f_analye.py
@@ -20,39 +20,16 @@ for variable in df.columns:
     if df[variable].dtypes == "object":
         print(df[variable].value_counts())
 
-# another comment
 
 print(df.columns)
 
-# create arrival date
-#df["arrival_date"] = date(df.arrival_date_year, df.arrival_date_month, df.arrival_date_day_of_month)
+# new arrival date variable in date format
+df['arrival_date'] = ((df.arrival_date_year.astype(str) +
+                       df.arrival_date_month +
+                       df.arrival_date_day_of_month.astype(str)
+                       )
+                      .pipe(pd.to_datetime, format="%Y%B%d")
+                      )
 
 
-#print(df.describe(include='all'))
-
-
-print(type(df["arrival_date_year"]))
-print(type(df.arrival_date_year))
-
-#print(df[['arrival_date_year', 'arrival_date_month', 'arrival_date_day_of_month']].head())
-#print(datetime.strptime("18March2009", "%d%B%Y").date())
-#date.strptime()
-print(df.arrival_date_month.unique())
-
-print(str(df.arrival_date_year.head()) + df.arrival_date_month.head())
-
-
-
-df['arrival_date'] = df.arrival_date_year.astype(str) + df.arrival_date_month + df.arrival_date_day_of_month.astype(str)
-df['arrival_date'] = pd.to_datetime(df.arrival_date, format="%Y%B%d")
-
-
-df['arrival_date'] = (df.arrival_date_year.astype(str) + df.arrival_date_month + df.arrival_date_day_of_month.astype(str)
-                      .pipe(pd.to_datetime, format="%Y%B%d"))
 print(df.arrival_date.head())
-#df['arrival_date'] = pd.to_datetime(df.arrival_date, format="%Y%B%d")
-print("hhhhhhhh")
-#print(str(df.arrival_date_year.head()) + df.arrival_date_month.head() + df.arrival_date.head())
-print(df.arrival_date.head())
-#pd.to_datetime()
-#print(df.arrival_date_day_of_month.astype(str).str.pad(width=2, side='left', fillchar='0'))
