6a5512d8cdb1df76783ae665bff31acd74075279
fjesser <felixjonas.esser@googlemail.com>
2020-05-21 20:53:59 +0200

    Es hat geklappt mit Spyder!!!

--- syntax/f_analyse.py
+++ syntax/f_analyse.py
@@ -1,14 +1,12 @@
 # Felix hotel demand analysis
 
+#%% Preparations
 
 # import modules
-import datetime as dt
+#import datetime as dt
 import pandas as pd
 import numpy as np
 import os
-
-
-
 
 #save folder in variablex
 file_folder = os.path.dirname(__file__)
@@ -16,13 +14,13 @@ file_folder = os.path.dirname(__file__)
 # load data
 df = pd.read_csv(os.path.join(file_folder, "..", "input", "hotel_bookings.csv"))
 
-#quit()
 
-
+#%% Inspect data
 # inspect (don't forget print)
-# df.info()  # structure in R
-# df.index  # gives rows (special python style)
-# df.columns  # gives columns
+
+#print(df.info())  # structure in R
+#print(df.index)  # gives rows (special python style)
+#print(df.columns)  # gives columns
 
 #print(df.columns) #print column names
 
@@ -32,11 +30,18 @@ df = pd.read_csv(os.path.join(file_folder, "..", "input", "hotel_bookings.csv"))
 # give frequencies of reservation_status
 #print(df["reservation_status"].value_counts())
 
+# subset 
+#print(df.loc[df["reservation_status"] == "Check-Out", 
+#             ["arrival_date", "reservation_status", 
+#              "reservation_status_date", "days_spent"]].head())
+
 
 #for variable in df.columns:
 #    if df[variable].dtypes == "object":
 #        print(df[variable].value_counts())
 
+
+#%% Data transformation
 
 # transform reservation_status_date to datetime object
 df['reservation_status_date'] = pd.to_datetime(df['reservation_status_date'])
@@ -62,11 +67,14 @@ df['cost'] = np.where(df['reservation_status'] == "Check-Out",
 # drop columns starting with arrival_date_
 df = df.loc[:, ~df.columns.str.startswith('arrival_date_')]
 
+
+
+#%% Save transformed data 
 #
 df.to_csv(os.path.join(file_folder, "..", "input", "hotel_bookings_FE.csv"))
 
 
-#print(df.loc[df["reservation_status"] == "Check-Out", ["arrival_date", "reservation_status", "reservation_status_date", "days_spent"]].head())
-#print(df.days_spent)
-#print(df.cost)
-#print(df.columns)
+
+print(df.days_spent)
+print(df.cost)
+print(df.columns)
