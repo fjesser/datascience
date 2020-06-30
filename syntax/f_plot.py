8d097013b8c1306edf6b43cb76f04a00a8318077
fjesser <felixjonas.esser@googlemail.com>
2020-06-30 19:49:32 +0200

    Modified own syntax and updated ideas.txt with new mission!

--- syntax/f_plot.py
+++ syntax/f_plot.py
@@ -46,13 +46,13 @@ cnf_matrix = metrics.confusion_matrix(df.is_canceled,
 cnf_matrix
 
 
-class_names=[0,1] # name  of classes
+class_names = [0, 1]  # name  of classes
 fig, ax = plt.subplots()
 tick_marks = np.arange(len(class_names))
 plt.xticks(tick_marks, class_names)
 plt.yticks(tick_marks, class_names)
 # create heatmap
-sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu" ,fmt='g')
+sns.heatmap(pd.DataFrame(cnf_matrix), annot=True, cmap="YlGnBu", fmt='g')
 ax.xaxis.set_label_position("top")
 plt.tight_layout()
 plt.title('Confusion matrix', y=1.1)
@@ -60,7 +60,6 @@ plt.ylabel('Actual label')
 plt.xlabel('Predicted label')
 
 plt.show()
-
 
 
 # %% Decision Tree
